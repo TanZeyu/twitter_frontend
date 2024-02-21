dd302567c2e1169b1bae9e17c7ae252243116082
TanZeyu <ztan56@outlook.com>
2024-02-21 15:20:49 -0800

    feat: 🎸 Added login page optimization using antd-mobile

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,28 +1,45 @@
 import { useState } from 'react';
+import { Button, Input, Form, PasscodeInput, NumberKeyboard, Dialog} from 'antd-mobile';
 import './index.css';
 
 
 const Login = () => {
-  const [name, setName] = useState();
-  const [pwd, setPwd] = useState();
+  // const [name, setName] = useState();
+  // const [pwd, setPwd] = useState();
+  const [form] = Form.useForm()
 
-  const clickHandler = () => {
-    alert("Login successfully!" + name + ", " + pwd);
+  const init_val = {
+    userName: "None",
+    password: "0000"
   }
-  const onChangeHandler = (e) => {
-    setName(e.target.value);
+  const onSubmit = (e) => {
+    const values = form.getFieldsValue()
+    Dialog.alert({
+      content: <pre>{JSON.stringify(values, null, 2)}</pre>,
+    })
   }
-  const onChangeHandlerPwd = (e) => {
-    setPwd(e.target.value);
-  }
-
   return (
     <div className="login">
-      {/* <p>This is Your username {name}</p>
-      <p>This is Your password {pwd}</p> */}
-      <div>User Name: <input onChange={onChangeHandler}/></div>
-      <div>Password: <input type = "password" onChange={onChangeHandlerPwd}/></div>
-      <div><button onClick={clickHandler}>Log in</button></div>
+      <Form layout='horizontal' 
+        form = {form}
+        mode='card' 
+        initialValues={init_val}
+        footer={
+          <Button block color='primary' onClick={onSubmit} size='large'>
+            Submit
+          </Button>
+        }
+      >
+
+        <Form.Item label='User name' name="userName">
+          <Input placeholder='Input user name here' clearable />
+        </Form.Item>
+
+        <Form.Item label='Password' name="password">
+          <Input placeholder='Input your password here' clearable type='password' />
+        </Form.Item>
+
+      </Form>
     </div>
   );
 }
